115758cc1774f98c452a07a073e65c2478130cfe
Adnanhrc <mohammadnan88@gmail.com>
2025-01-22 09:23:42 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust the path based on your project structure
+    './public/index.html', // Include any other relevant files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
